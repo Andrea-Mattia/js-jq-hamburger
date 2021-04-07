91282e14d2fb1cf917e693b4c398f74e01cc91e5
Andrea-Mattia <andrea.mattia94@gmail.com>
2021-04-07 16:56:59 +0200

    add solution

--- main.js
+++ main.js
@@ -1,2 +1,21 @@
-console.log('Works!');
-console.log('jQuery', $);
+/**
+ * Hamburger menu
+ * Mostrare / nascondere il menu principale (ricordate che per vedere
+ * l’hamburger in alto a destra dovete scendere a risoluzioni tipo tablet
+ * mobile).
+ */
+
+// REFS
+var bars = $('.fa-bars');
+var menu = $('.hamburger-menu');
+var closeMenu = $('.close');
+
+// Dispaying the menu
+bars.click( function() {
+    menu.fadeIn();
+});
+
+// Closing the Menu
+closeMenu.click( function() {
+    menu.fadeOut();
+});
